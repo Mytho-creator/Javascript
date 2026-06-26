03736db3e8113fe5fe8e39533fab78675eed73cd
Mytho-creator <monikasahu2656@gmail.com>
2026-06-26 11:25:27 +0000

    learn comparision operators

---
+++ 01_basics/05_comparision.js
@@ -0,0 +1,16 @@
+// console.log(2 == 2);
+// console.log(2 == "2");
+// console.log(2 === "2");
+// console.log(5>=4);
+// console.log(8<=9);
+
+// console.log(null>0)
+// console.log(null == 0)
+// console.log(null<0)
+// console.log(null>=0) // true
+
+// console.log(undefined==0)
+// console.log(undefined>=0)
+// console.log(undefined==null) //true
+// console.log(undefined>null)
+console.log(undefined===null)  //false
