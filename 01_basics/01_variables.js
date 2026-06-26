982cdb24dbf25c8e7a2b28e186998fd81a37544a
Mytho-creator <monikasahu2656@gmail.com>
2026-06-26 09:31:29 +0000

    basics of js learning

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+
+const accountId = 123454
+const userName = "Monika"
+let  accountemail = "moni1234@gmail.com"
+var password = "5556677"
+
+city = "Delhi"
+
+console.table([userName,accountId,accountemail,password,city])
+
+/*  
+prefer not to use var because of issue in block scope and functional scope
+
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+4);  we are using node.js , not browser
+
+// Primitive data types
+
+// number => range : 2 to the power 53
+// bigint => large numbers
+// string => ""
+//boolean => true/ false
+// null => statandlon value
+// undefined => 
+
+// symbol => use in react
+
+// Non - Primitive data types
+//objects
