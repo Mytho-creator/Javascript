529825ff89bcc3e0bd839f597ab89da8cd17987c
Mytho-creator <monikasahu2656@gmail.com>
2026-06-26 10:26:22 +0000

    conversion learning

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,10 @@
+
+// console.log(2%5);
+// console.log(2%3);
+
+console.log(1+"2");
+console.log("1"+ "2");
+console.log("Monika" + "Sahu")
+console.log("1"+2);
+console.log("1"+2+2);
+console.log(1+2+"2");
