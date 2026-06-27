1c90318195ec24a2c7d26be60ee5f7c0e96f7487
Mytho-creator <monikasahu2656@gmail.com>
2026-06-27 15:57:11 +0000

    learn basics of array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,60 @@
+const myArr = [1, 2,3,4,"Apple",6]
+// console.log(myArr[4]);
+// console.log(typeof myArr);
+
+const heroes = ["Shaktiman","Superman"]
+
+const myArr2 = new Array(5,10, 15)
+// console.log(typeof myArr2);
+
+// console.log(myArr2);
+
+// ************* Array methods  *******************
+
+myArr.push("Orange","grapes")
+// console.log(myArr);
+myArr.pop()
+// console.log(myArr);
+
+// myArr.shift();
+// myArr.unshift("Mango")
+// console.log(myArr.includes("Orange"));
+// console.log(myArr.includes("guava"));
+// console.log(myArr.includes("Grapes"));
+// console.log(myArr.indexOf("Orange"));
+
+// const newArr = myArr.join()
+// console.log(newArr);
+// console.log(typeof newArr);
+
+// slice and splice
+
+// console.log("A",myArr);
+// const myn1 = myArr.slice(0, 3)
+// console.log(myn1);
+// console.log(myArr);
+const myn2 = myArr.splice(4, 7)
+ console.log(myn2);
+ console.log(myArr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
