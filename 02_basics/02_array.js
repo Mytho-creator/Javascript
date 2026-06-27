63ec0f1e702e3950c05cc7e10031e3d19ca43a89
Mytho-creator <monikasahu2656@gmail.com>
2026-06-27 17:06:53 +0000

    learn advance arrays

---
+++ 02_basics/02_array.js
@@ -0,0 +1,20 @@
+const vegies = ["Potatos","Tomatoes","peas","Onion"]
+const fruits = ["Mango","Apple","Orange"]
+
+// vegies.push(fruits);
+// const allItems = vegies.concat(fruits)
+// const allItems = [...vegies,...fruits]
+// console.log(allItems);
+
+// const nestedArray = [1, 2, 3,[4,5,6,[7, 8, 9]]]
+// const anotherArray = nestedArray.flat(Infinity)
+// console.log(anotherArray);
+
+// console.log(Array.isArray("fruits"));
+// console.log(Array.from("fruits"));
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+console.log(Array.of(score1,score2,score3));
+
