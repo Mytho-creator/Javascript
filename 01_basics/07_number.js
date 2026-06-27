7adb5de25e576da89273366e79d06e5442bb6d2e
Mytho-creator <monikasahu2656@gmail.com>
2026-06-27 06:02:45 +0000

    learn math and num

---
+++ 01_basics/07_number.js
@@ -0,0 +1,36 @@
+const score = 200;
+// console.log(score)
+
+const balance = new Number(100)
+console.log(balance)
+// console.log(balance.toString().length)
+// console.log(typeof balance)
+
+// console.log(balance.toFixed(2))
+
+const otherNum = 123.678
+// console.log(otherNum.toPrecision(3))
+
+const hundreds = 100000;
+// console.log(hundreds.toLocaleString('en-IN'))
+
+//************** Maths ************* */
+
+// console.log(Math);
+// console.log(Math.abs(-8))
+// console.log(Math.abs(8))
+// console.log(Math.round(8.3))
+// console.log(Math.round(8.6))
+// console.log(Math.ceil(8.3))
+// console.log(Math.floor(8.6))
+// console.log(Math.pow(19,2))
+// console.log(Math.sqrt(361))
+// console.log(Math.random())
+// console.log(Math.floor(Math.random()*10)+1)
+
+const min = 10;
+const max = 20;
+
+//  
+
+
