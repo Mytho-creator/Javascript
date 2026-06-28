78eaedea6bc2279d3e29196e4ee58e6459108d71
Mytho-creator <monikasahu2656@gmail.com>
2026-06-28 15:48:33 +0000

    Learn basic loops

---
+++ 05_Loops/one.js
@@ -0,0 +1,24 @@
+
+
+// for (let i = 1; i <= 10; i++) {
+//         if(i == 5){
+//         break;
+//     }
+//     console.log(i);   
+// }
+// for (let i = 1; i <= 10; i++) {
+//         if(i == 5){
+//         continue;
+//     }
+//     console.log(i);   
+// }
+let i = 1
+// while (i<=5) {
+//     console.log(i);
+//     i++   
+// }
+
+do {
+    console.log(i);
+    i++
+} while (i<=5);
